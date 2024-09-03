fa45edfb3b866492bac3a8ee02a614670f83b2d6
ferrauto1020 <ferrauto1020@gmail.com>
2024-09-03 16:28:25 +0200

    feat(MainPage/MenuItems): displayed the menu items

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,18 +4,13 @@ namespace RestaurantApp;
 
 public partial class App : Application
 {
-	private readonly DatabaseService _databaseService;
-	public App(DatabaseService databaseService)
+		public App(DatabaseService databaseService)
 	{
 		InitializeComponent();
-		_databaseService = databaseService;
 		MainPage = new AppShell();
+
+		Task.Run (async() =>
+		await databaseService.InitializeDatabaseAsync()).GetAwaiter().GetResult();
 	}
 
-	protected override async void OnStart()
-	{
-		base.OnStart();
-		await _databaseService.InitializeDatabaseAsync();
-		
-	}
 }
--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -2,7 +2,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using RestaurantApp.Data;
 using RestaurantApp.Models;
-
+using MenuItem = RestaurantApp.Data.MenuItem;
 namespace RestaurantApp.ViewModels
 {
     public partial class HomeViewModel : ObservableObject
@@ -10,6 +10,8 @@ namespace RestaurantApp.ViewModels
         private readonly DatabaseService _databaseService;
         [ObservableProperty]
         private MenuCategoryModel[] _categories = [];
+        [ObservableProperty]
+        private MenuItem[] _menuItems = [];
         [ObservableProperty]
         private MenuCategoryModel? _selectedCategory = null;
         [ObservableProperty]
@@ -29,19 +31,24 @@ namespace RestaurantApp.ViewModels
             Categories = (await _databaseService.GetMenuCategoriesAsync()).Select(MenuCategoryModel.FromEntity).ToArray();
             Categories[0].IsSelected = true;
             SelectedCategory = Categories[0];
+            MenuItems = await _databaseService.GetMenuItemsByCategoryAsync(SelectedCategory.Id);
             IsLoading = false;
         }
 
         [RelayCommand]
-        private void SelectCategory(int categoryId)
+        private async Task SelectCategoryAsync(int categoryId)
         {
             if (SelectedCategory.Id == categoryId)
                 return;
+            IsLoading = true;
             var existingSelectedCategory = Categories.First(c => c.IsSelected);
             existingSelectedCategory.IsSelected = false;
             var newSelectedCategory = Categories.First(c => c.Id == categoryId);
             newSelectedCategory.IsSelected = true;
+
             SelectedCategory = newSelectedCategory;
+            MenuItems = await _databaseService.GetMenuItemsByCategoryAsync(SelectedCategory.Id);
+            IsLoading = false;
         }
     }
 }
