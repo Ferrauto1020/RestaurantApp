31660ab56d9c8af4cd1c9b8755ffe518d833b12e
ferrauto1020 <ferrauto1020@gmail.com>
2024-09-03 11:45:10 +0200

    feat(UI - bug fixing): start to work on UI and trying to push most code to find when it bugs

--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/RestaurantApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/RestaurantApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b715075c8f43ff8e053bcbc1c0b077a78b92066b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+12950be7a5ac2d42733f20df4fe78bb70a92d155")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
