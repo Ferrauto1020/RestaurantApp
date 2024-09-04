4a624d27e06b82e8d4d185d45c99cb06eb11c9fa
ferrauto1020 <ferrauto1020@gmail.com>
2024-09-04 11:08:05 +0200

    feat(HomeViemModel/CartItems): created method for make dinamic the bill and added in the UI

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using RestaurantApp.Data;
@@ -19,11 +20,26 @@ namespace RestaurantApp.ViewModels
         public ObservableCollection<CartModel> CartItems { get; set; } = new();
         [ObservableProperty]
         private bool _isLoading;
-
+        [ObservableProperty, NotifyPropertyChangedFor(nameof(TaxAmount)), NotifyPropertyChangedFor(nameof(Total))]
+        private decimal _subtotal;
+        [ObservableProperty, NotifyPropertyChangedFor(nameof(TaxAmount)), NotifyPropertyChangedFor(nameof(Total))]
+        private int _taxPercentage;
+        public decimal TaxAmount => (Subtotal * TaxPercentage) / 100;
+        public decimal Total => Subtotal + TaxAmount;
         public HomeViewModel(DatabaseService databaseService)
         {
             _databaseService = databaseService;
+            CartItems.CollectionChanged += CartItems_CollectionChanged;
         }
+
+        private void CartItems_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            //it will be executed anytime we are adding any items to the cart
+            //removing items from the cart
+            //clearing the cart
+            RecalculateAmounts();
+        }
+
         private bool _isInitialized;
         public async ValueTask InitializeAsync()
         {
@@ -73,12 +89,16 @@ namespace RestaurantApp.ViewModels
             {
                 //item already in cart, increase the quantity
                 cartItem.Quantity++;
-
+                RecalculateAmounts();
             }
         }
 
         [RelayCommand]
-        private void IncreaseQuantity(CartModel cartItem) => cartItem.Quantity++;
+        private void IncreaseQuantity(CartModel cartItem)
+        {
+            cartItem.Quantity++;
+            RecalculateAmounts();
+        }
 
         [RelayCommand]
         private void DecreaseQuantity(CartModel cartItem)
@@ -86,9 +106,15 @@ namespace RestaurantApp.ViewModels
             cartItem.Quantity--;
             if (cartItem.Quantity == 0)
                 CartItems.Remove(cartItem);
+            else
+                RecalculateAmounts();
         }
         [RelayCommand]
         private void RemoveItemFromCart(CartModel cartItem) => CartItems.Remove(cartItem);
 
+        private void RecalculateAmounts()
+        {
+            Subtotal = CartItems.Sum(c => c.Amount);
+        }
     }
 }
--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/RestaurantApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/RestaurantApp.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e20f394f38c0ca7b9aedf2541d5a3ca6d2411b9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+af90945032325c2ed7d8fe29f18b39ce3a8057f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
